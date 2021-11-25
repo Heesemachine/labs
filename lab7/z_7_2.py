94af54e5f30c27d0ef167dbd6ca7c14bb42cada8
sqwkez <93590785+andriysvyda@users.noreply.github.com>
2021-11-25 14:30:31 +0200

    ,

--- lab7/z_7_2.py
+++ lab7/z_7_2.py
@@ -3,11 +3,10 @@ import random
 n = int(input("рядки : "))
 m = int(input("стовпці : "))
 sum = 0
-a = [[random.randrange(-100, 100) for j in range(m)] for i in range(n)]
+a = [[(1/(i+j)) for j in range(1,m)] for i in range(1,n)]
 print(*a, sep="\n")
 for i in range(len(a)):
     for j in range(len(a)):
-        if i%2 == 0 and j%2 == 0:
-            if a[i][j] < 0:
-                sum += 1
+        if (i+j)%2 == 1:
+            sum += a[i][j]
 print(sum)
