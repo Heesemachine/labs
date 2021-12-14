b4fb24330583754bbce5eb443e0b2b4bcd77aa30
sqwkez <93590785+andriysvyda@users.noreply.github.com>
2021-12-14 15:30:59 +0200

    ','

--- lab6/z_6_2.py
+++ lab6/z_6_2.py
@@ -1,10 +1,11 @@
 n = int(input("Введіть довжину масиву : "))
 a = []
-s = sum = 1
+suma = 0
+s = 1
 for i in range(1,n+1) :
-    s = s*((-1)**i)*i
-    a.append(s/i)
-    if a[i-1] > 0 :
-        sum = sum + a[i-1]
-print("сума = {0}".format(sum))
-
+    s *= ((-1)**i)*i
+    a.append(s/n)
+    if a[i - 1] > 0:
+        suma += a[i - 1]
+    print(a)
+print("сума = {0}".format(suma))
